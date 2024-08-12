c82c4970b55ce1b0fa11f2f5e78c9c818686b763
徐浩 <zyuhao7@yeah.net>
2024-08-12 17:06:56 +0800

    服务器业务逻辑代码

---
+++ src/server/chatserver.cc
@@ -0,0 +1,50 @@
+#include "chatserver.hpp"
+#include "json.hpp"
+#include "chatservice.hpp"
+
+#include <iostream>
+#include <functional>
+#include <string>
+using namespace std;
+using namespace placeholders;
+using json = nlohmann::json;
+
+ChatServer::ChatServer(EventLoop *loop,
+                       const InetAddress &listenAddr,
+                       const string &nameArg)
+    : _server(loop, listenAddr, nameArg), _loop(loop)
+{
+    _server.setConnectionCallback(std::bind(&ChatServer::onConnection, this, _1));
+
+    _server.setMessageCallback(std::bind(&ChatServer::onMessage, this, _1, _2, _3));
+
+    _server.setThreadNum(4);
+}
+
+void ChatServer::start()
+{
+    _server.start();
+}
+
+void ChatServer::onConnection(const TcpConnectionPtr &conn)
+{
+    if (!conn->connected())
+    {
+        ChatService::instance()->clientCloseException(conn);
+        conn->shutdown();
+    }
+}
+
+void ChatServer::onMessage(const TcpConnectionPtr &conn,
+                           Buffer *buffer,
+                           Timestamp time)
+{
+    string buf = buffer->retrieveAllAsString();
+
+    cout << buf << endl;
+
+    json js = json::parse(buf);
+
+    auto msgHandler = ChatService::instance()->getHandler(js["msgid"].get<int>());
+    msgHandler(conn, js, time);
+}
