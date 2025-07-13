e02624c048e98b169081d54851839cc9b52b2ffe
myh <zyuhao7@yeah.net>
2025-07-13 18:45:37 +0800

    数据库管理类

---
+++ include/server/db/db.h
@@ -0,0 +1,41 @@
+#ifndef DB_H
+#define DB_H
+#include<muduo/base/Logging.h>
+#include <string>
+#include <mysql/mysql.h>
+using namespace std;
+
+// 数据库配置信息
+static string server = "127.0.0.1";
+static string user = "root";
+static string password = "123456";
+static string dbname = "chat";
+
+// 数据库操作类
+class MySQL
+{
+public:
+    // 初始化数据库连接
+    MySQL()
+    {
+        _conn = mysql_init(nullptr);
+    }
+     // 释放数据库连接资源
+
+    ~MySQL()
+    {
+        if (_conn != nullptr)
+            mysql_close(_conn);
+    }
+    // 连接数据库
+    bool connect(); 
+    // 更新操作
+    bool update(string sql);
+    // 查询操作
+    MYSQL_RES* query(string sql);
+
+private:
+    MYSQL *_conn;
+};
+
+#endif
