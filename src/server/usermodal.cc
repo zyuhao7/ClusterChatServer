e1fd5e375ee9f4153521ec857b21ade337a6f748
myh <zyuhao7@yeah.net>
2025-07-15 21:48:32 +0800

    更新数据库状态信息

--- src/server/usermodal.cc
+++ src/server/usermodal.cc
@@ -22,3 +22,46 @@ bool UserModal::Insert(User& user)
     }
     return false;
 }
+
+User UserModal::query(int id)
+{
+      // 1. 组装 sql 语句
+    char sql[1024] = {0};
+    sprintf(sql, "select * from user where id = %d", id);
+
+    MySQL mysql;
+    if(mysql.connect())
+    {
+        MYSQL_RES* res = mysql.query(sql);
+        if(res != nullptr)
+        {
+           MYSQL_ROW row =  mysql_fetch_row(res);
+           if(row != nullptr)
+           {
+                User user;
+                user.SetId(atoi(row[0]));
+                user.SetName(row[1]);
+                user.SetPwd(row[2]);
+                user.SetState(row[3]);
+                mysql_free_result(res);
+                return user;
+           }
+        }
+    }
+    return  User();
+}
+
+bool UserModal::updateState(User& user)
+{
+     // 1. 组装 sql 语句
+    char sql[1024] = {0};
+    
+    sprintf(sql, "update user set state = '%s' where id = %d", user.GetState().c_str(), user.GetId());
+    MySQL mysql;
+    if(mysql.connect())
+    {
+       if(mysql.update(sql))
+            return true;
+    }
+    return  false;
+}
