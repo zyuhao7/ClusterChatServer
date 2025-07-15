640336c327f2fcdc9783ca229bd5ade84972e189
myh <zyuhao7@yeah.net>
2025-07-15 21:46:17 +0800

    在线用户map表

--- include/server/chatservice.hpp
+++ include/server/chatservice.hpp
@@ -26,13 +26,19 @@ public:
     void reg(const TcpConnectionPtr &conn, json &js, Timestamp time);
     // 获得消息对应的处理器
     MsgHandler getHandler(int msg_id);
+    // 客户端异常退出
+    void clientCloseException(const TcpConnectionPtr& conn);
 private:
     ChatService();
     // 存储消息id和其对应的业务处理方法
     unordered_map<int, MsgHandler> _msgHandlerMap;
-
+    // 存储在线用户的通信连接
+    unordered_map<int, TcpConnectionPtr> _userConnMap;
     // 数据操作类对象
     UserModal _userModal;
+
+    // 定义互斥锁, 保证 _userConnMap线程安全
+    mutex _mtx;
 };
 
 #endif
