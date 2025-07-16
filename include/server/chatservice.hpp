ea0a985dd27d2648ceb48df1f390b57cf14e92d9
myh <zyuhao7@yeah.net>
2025-07-16 22:22:39 +0800

    增加离线消息处理模型

--- include/server/chatservice.hpp
+++ include/server/chatservice.hpp
@@ -5,6 +5,7 @@
 #include <functional>
 #include <mutex>
 #include "json.hpp"
+#include "offlinemsgmodal.hpp"
 #include "usermodal.hpp"
 using namespace std;
 using namespace muduo;
@@ -24,6 +25,8 @@ public:
     void login(const TcpConnectionPtr &conn, json &js, Timestamp time);
     // 处理注册业务
     void reg(const TcpConnectionPtr &conn, json &js, Timestamp time);
+      // 处理一对一聊天业务
+    void oneChat(const TcpConnectionPtr &conn, json &js, Timestamp time);
     // 获得消息对应的处理器
     MsgHandler getHandler(int msg_id);
     // 客户端异常退出
@@ -36,7 +39,8 @@ private:
     unordered_map<int, TcpConnectionPtr> _userConnMap;
     // 数据操作类对象
     UserModal _userModal;
-
+    // 离线消息模型
+    OfflineMsgModal _offlineMsgModal;
     // 定义互斥锁, 保证 _userConnMap线程安全
     mutex _mtx;
 };
