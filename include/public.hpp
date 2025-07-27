ad18846528b4a4610ebbf4dd48135b91b508e401
myh <zyuhao7@yeah.net>
2025-07-27 11:09:34 +0800

    enumClass

--- include/public.hpp
+++ include/public.hpp
@@ -6,20 +6,20 @@ server和client的公共文件
 */
 enum EnMsgType
 {
-    LOGIN_MSG = 1, // 登录消息
-    LOGIN_MSG_ACK, // 登录响应消息
-    LOGINOUT_MSG, // 注销消息
-    REG_MSG, // 注册消息
-    REG_MSG_ACK, // 注册响应消息
-    ONE_CHAT_MSG, // 聊天消息
+    LOGIN_MSG = 1,  // 登录消息
+    LOGIN_MSG_ACK,  // 登录响应消息
+    LOGINOUT_MSG,   // 注销消息
+    REG_MSG,        // 注册消息
+    REG_MSG_ACK,    // 注册响应消息
+    ONE_CHAT_MSG,   // 聊天消息
     ADD_FRIEND_MSG, // 添加好友消息
 
-    CREATE_GROUP_MSG, // 创建群组
+    CREATE_GROUP_MSG,     // 创建群组
     CREATE_GROUP_MSG_ACK, // 创建群组响应消息
-    ADD_GROUP_MSG, // 加入群组
-    ADD_GROUP_MSG_ACK, // 加入群组响应消息
-    GROUP_CHAT_MSG, // 群聊天
-    
+    ADD_GROUP_MSG,        // 加入群组
+    ADD_GROUP_MSG_ACK,    // 加入群组响应消息
+    GROUP_CHAT_MSG,       // 群聊天
+
 };
 
 #endif
