63fc21adb50773e740bdbc75f0dfc1a12817c118
myh <zyuhao7@yeah.net>
2025-07-27 16:01:26 +0800

    add Redis obj

--- include/server/chatservice.hpp
+++ include/server/chatservice.hpp
@@ -9,6 +9,7 @@
 #include "usermodal.hpp"
 #include "friendmodal.hpp"
 #include "groupmoodal.hpp"
+#include "redis.hpp"
 using namespace std;
 using namespace muduo;
 using namespace muduo::net;
@@ -21,43 +22,47 @@ using MsgHandler = std::function<void(const TcpConnectionPtr &conn, json &js, Ti
 class ChatService
 {
 public:
-    // 获取单例对象的接口函数
-    static ChatService *instance();
-    // 处理登录业务
-    void login(const TcpConnectionPtr &conn, json &js, Timestamp time);
-    // 处理注册业务
-    void reg(const TcpConnectionPtr &conn, json &js, Timestamp time);
-      // 处理一对一聊天业务
-    void oneChat(const TcpConnectionPtr &conn, json &js, Timestamp time);
-    // 获得消息对应的处理器
-    MsgHandler getHandler(int msg_id);
-    // 客户端异常退出
-    void clientCloseException(const TcpConnectionPtr& conn);
-    // 服务异常, 重置用户状态
-    void reset();
-    // 添加好友服务
-    void addFriend(const TcpConnectionPtr& conn, json& js, Timestamp time);
-    // 创建群组服务
-    void createGroup(const TcpConnectionPtr& conn, json& js, Timestamp time); 
-    // 加入群组服务
-    void AddGroup(const TcpConnectionPtr& conn, json& js, Timestamp time);
-    // 群聊天服务
-    void groupChat(const TcpConnectionPtr& conn, json& js, Timestamp time);
+  // 获取单例对象的接口函数
+  static ChatService *instance();
+  // 处理登录业务
+  void login(const TcpConnectionPtr &conn, json &js, Timestamp time);
+  // 处理注册业务
+  void reg(const TcpConnectionPtr &conn, json &js, Timestamp time);
+  // 处理一对一聊天业务
+  void oneChat(const TcpConnectionPtr &conn, json &js, Timestamp time);
+  // 获得消息对应的处理器
+  MsgHandler getHandler(int msg_id);
+  // 客户端异常退出
+  void clientCloseException(const TcpConnectionPtr &conn);
+  // 服务异常, 重置用户状态
+  void reset();
+  // 添加好友服务
+  void addFriend(const TcpConnectionPtr &conn, json &js, Timestamp time);
+  // 创建群组服务
+  void createGroup(const TcpConnectionPtr &conn, json &js, Timestamp time);
+  // 加入群组服务
+  void AddGroup(const TcpConnectionPtr &conn, json &js, Timestamp time);
+  // 群聊天服务
+  void groupChat(const TcpConnectionPtr &conn, json &js, Timestamp time);
+
 private:
-    ChatService();
-    // 存储消息id和其对应的业务处理方法
-    unordered_map<int, MsgHandler> _msgHandlerMap;
-    // 存储在线用户的通信连接
-    unordered_map<int, TcpConnectionPtr> _userConnMap;
-    
-    // 数据操作类对象
-    UserModal _userModal;
-    OfflineMsgModal _offlineMsgModal;
-    FriendModal _friendModal;
-    GroupModal _groupModal;
-    
-    // 定义互斥锁, 保证 _userConnMap线程安全
-    mutex _mtx;
+  ChatService();
+  // 存储消息id和其对应的业务处理方法
+  unordered_map<int, MsgHandler> _msgHandlerMap;
+  // 存储在线用户的通信连接
+  unordered_map<int, TcpConnectionPtr> _userConnMap;
+
+  // 数据操作类对象
+  UserModal _userModal;
+  OfflineMsgModal _offlineMsgModal;
+  FriendModal _friendModal;
+  GroupModal _groupModal;
+
+  // 定义互斥锁, 保证 _userConnMap线程安全
+  mutex _mtx;
+
+  // redis操作对象
+  Redis _redis;
 };
 
 #endif
