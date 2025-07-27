7c0286415bce7ec62ac580a280f487a9d12702af
myh <zyuhao7@yeah.net>
2025-07-27 11:04:54 +0800

    add Notes4group

--- include/server/modal/group.hpp
+++ include/server/modal/group.hpp
@@ -6,24 +6,38 @@
 #include "groupuser.hpp"
 using namespace std;
 
+/// @file group.hpp
+/// @brief 定义群组类，用于表示群组的基本信息和成员管理
+
+/// @class Group
+/// @brief 群组类，包含群组的ID、名称、描述以及成员列表
+///
+/// 该类提供了对群组基本信息的设置和获取方法，同时维护了一个群组成员的列表。
+/// 成员列表由 GroupUser 类型的对象组成。
+///
+/// 功能包括：
+/// - 设置和获取群组的ID、名称、描述
+/// - 获取群组成员列表
+
 class Group
 {
- public:
-    Group(int id = -1, string name = "", string desc ="")
-        :id_(id),
-        name_(name),
-        desc_(desc)
-    {}
-    void SetId(int id){id_ = id;}
-    void SetName(string name){name_ = name;}
-    void SetDesc(string desc){desc_ = desc;}
+public:
+    Group(int id = -1, string name = "", string desc = "")
+        : id_(id),
+          name_(name),
+          desc_(desc)
+    {
+    }
+    void SetId(int id) { id_ = id; }
+    void SetName(string name) { name_ = name; }
+    void SetDesc(string desc) { desc_ = desc; }
 
-    int GetId() {return id_;}
-    string GetName(){return name_;}
-    string GetDesc(){return desc_;}
-    vector<GroupUser>& GetUsers(){return users;}
+    int GetId() { return id_; }
+    string GetName() { return name_; }
+    string GetDesc() { return desc_; }
+    vector<GroupUser> &GetUsers() { return users; }
 
- private:
+private:
     int id_;
     string name_;
     string desc_;
@@ -31,4 +45,3 @@ class Group
 };
 
 #endif
-
